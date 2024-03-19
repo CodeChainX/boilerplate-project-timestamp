b887b133cfc5c1564385bc6303ca61f5a30d8fd8
weshare237 <fopaduclair2000@gmail.com>
2024-03-19 18:13:56 +0100

    Timestamp Microservice

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/:date?', (req, res) => {
+  const date = req.params.date;
+  let unixTimestamp, utcString;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (date) {
+    const parsedDate = new Date(date);
+    if (isNaN(parsedDate.getTime())) {
+      return res.json({ error: 'Invalid Date' });
+    }
+    unixTimestamp = parsedDate.getTime();
+    utcString = parsedDate.toUTCString();
+  } else {
+    const now = new Date();
+    unixTimestamp = now.getTime();
+    utcString = now.toUTCString();
+  }
+
+  res.json({ unix: unixTimestamp, utc: utcString });
 });
 
-
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
